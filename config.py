85db95cf4655a3c670612d8779cdf5e7f67d7b26
Joon Rhee <coldplay746@gmail.com>
2024-03-17 17:14:12 -0600

    add sql-id

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 polygonAPIkey = 'YpUrkNsnEiLdKGJMpcc5k8vE1tdIrger'
 
 ALPHA_VANTAGE_API_KEY = '6R__ekvBhI0FVUHnO5SI4A'
+
+hostname = "datafor.mysql.database.azure.com"
+username = "admin1"
+password = "Earn1ngs"
+port = 3306
+database_name = "final_stock"
